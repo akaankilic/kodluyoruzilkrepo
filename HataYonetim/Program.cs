0d5443df83412700a56a835f75daf403192f2ae0
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-11-25 22:01:50 +0300

    hata yonetim kursu derste işlenenler

---
+++ HataYonetim/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HataYonetim;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       try
+       {
+        Console.WriteLine("Bir Sayı Giriniz ...");
+        int sayi =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Girdiğiniz sayı "+sayi);
+       }
+       catch (System.Exception expsiton)
+       {
+        Console.WriteLine("Hata "+expsiton.Message.ToString());
+        
+       }
+       //finally{
+       // Console.WriteLine("İşlem Tamamlandı");
+       //}
+       try
+       {
+        //int a = int.Parse(null);
+       // int a = int.Parse("ad");
+        int a = int.Parse("32132132132");
+       }
+       catch (Exception ex)
+       {
+        Console.WriteLine("Yanlış veri tipi..");
+        Console.WriteLine(ex);
+       }
+        
+    }
+}
