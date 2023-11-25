d95d4153f0a58ef5e60d2315d96138921cd1ced1
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-11-26 01:21:59 +0300

    ifElseYapıları
    if Else ve elseif yapısı

---
+++ IfElseYapıları/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfElseYapıları;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int zaman=DateTime.Now.Hour;
+
+        if (zaman <= 12&& zaman>=5 )
+        
+            Console.WriteLine("Günaydın..");
+        else if(zaman<=18 &&zaman>=12){
+            Console.WriteLine("İyi Günler..");
+        }else if (zaman>=01&&zaman<=5)
+        {
+            Console.WriteLine("İyi Geceler..");
+        }else{
+            Console.WriteLine("İyi Akşamlar..");
+        }
+    }
+}
